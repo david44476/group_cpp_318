0c0e7bc1787408f55a99b26a1065c883e7afb9fb
david44476 <davidov435@mail.ru>
2025-05-25 07:12:54 +0900

    Delete Lab_4 directory

--- Lab_4/task_2.h
+++
@@ -1,5 +0,0 @@
-#ifndef TASK_2_H
-#define TASK_2_H
-auto Task_2a () -> void;
-auto Task_2b () -> void;
-#endif // TASK_2_H
--- Lab_4/type_thecking.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-auto Type_Thecking (short a, short min, short max) -> short;  // проверка на коректность ввода
-auto Type_Thecking (short a) -> short;  // проверка на коректность ввода
-auto Type_Thecking (char c) -> char;  // проверка на коректность ввода
-#endif // FUNCTIONS_H
