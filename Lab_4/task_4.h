529063ebd57185a23f21e38e0090affc136a1ff5
david44476 <davidov435@mail.ru>
2025-05-25 07:14:15 +0900

    Add files via upload

---
+++ Lab_4/task_4.h
@@ -0,0 +1,6 @@
+#ifndef TASK_4_H
+#define TASK_4_H
+auto FacItr (short a) -> unsigned long long;  // функция для нахождения Факториала с помощью итерации
+auto FacRec (short a) -> unsigned long long;  // функция для нахождения Факториала с помощью рекурсии
+auto fibonacci() -> void;  // функция для нахождения число Фибоначчи с помощью итерации
+#endif // TASK_4_H
