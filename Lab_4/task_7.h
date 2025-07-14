deea7b477d9723dd5f863634e717426a44029b40
david44476 <davidov435@mail.ru>
2025-07-14 16:45:48 +0900

    Delete Lab_4 directory

--- Lab_4/task_7.h
+++
@@ -1,8 +0,0 @@
-#ifndef TASK_7_H
-#define TASK_7_H
-constexpr unsigned short size{4};
-auto Faza() -> void;
-void printArray(char(&ref_array)[size]);
-void swap(char(&ref_array)[size], int i, int j);
-bool shifts(char(&ref_array)[size]);
-#endif // TASK_7_H
