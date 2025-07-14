5b04925041d2995590a231dd5614c7db7a7d107f
david44476 <davidov435@mail.ru>
2025-07-14 15:28:13 +0900

    Delete Lab_4 directory

--- Lab_4/type_thecking.h
+++
@@ -1,8 +0,0 @@
-#ifndef TYPE_THECKING_H
-#define TYPE_THECKING_H
-// функции для проверки ввода
-auto Type_Thecking (short a, short min, short max) -> short;
-auto Char_Type_Thecking (char c) -> char;
-auto Type_Thecking (double a) -> double;
-auto Type_Thecking (short a) -> short;
-#endif // TYPE_THECKING_H
