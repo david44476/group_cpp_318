ac9bb6d2cfde88196c1a062dbe55e4aaeb271452
david44476 <davidov435@mail.ru>
2025-07-15 20:43:28 +0900

    Update task_7.cpp

--- Lab_4/task_7.cpp
+++ Lab_4/task_7.cpp
@@ -1,14 +1,35 @@
 #include<iostream>
-#include <algorithm>// содержит прототип функции sort()
+#include <algorithm>
 #include"task_7.h"
 
 auto Faza() -> void {
-    char fazaArray[size] = {'A','B','C','N'};
-    // Упорядочиваем при помощи библиотечной  функции
-    //std::sort(std::begin(fazaArray), std::end(fazaArray));
 
-    for (auto& i : fazaArray) {
-        std::cout << i;
+    char fazaArray[size] = {'A','B','C','N'};
+
+    std::cout << "Начальное подключении обмоток для 3х-фазного двигателя A, B, C, N ; где N нулевой провод" << '\n';
+    for (auto i{0}; i < size -1; ++i) {
+        std::cout << "фаза " << fazaArray[i] << '\n';
     }
-    std::cout << '\n';
+    std::cout << "нулевой провод " << fazaArray[3] << '\n';
+
+    std::cout << "варианты подключении обмоток для 3х-фазного двигателя" << '\n';
+    for (auto i{0}; i < size; ++i) {
+        swap(fazaArray);
+        print(fazaArray);
+    }
+}
+
+auto swap(char(&ref_array)[size]) -> void {
+    for (auto i{0}, j{1}; i < size - 1; ++i, ++j) {
+        char s = ref_array[i];
+        ref_array[i] = ref_array[i];
+        ref_array[i] = s;
+    }
+}
+
+auto print (char(&ref_array)[size]) -> void {
+    for (auto k : ref_array) {
+        std::cout << k << ' ';
+    }
+    std::cout << std::endl;
 }
