82546245132b20f99e65d20337f64b4403294105
david44476 <davidov435@mail.ru>
2025-07-27 01:44:02 +0900

    Add files via upload

---
+++ Lab_4/type_thecking.h
@@ -0,0 +1,11 @@
+#ifndef TYPE_THECKING_H
+#define TYPE_THECKING_H
+// функции для проверки ввода
+auto Type_Thecking (short a, short min, short max) -> short;
+auto Char_Type_Thecking (char c) -> char;
+auto Type_Thecking (double a) -> double;
+auto Type_Thecking (short a) -> short;
+auto Type_Thecking (unsigned int a) -> unsigned int;
+auto Type_Thecking (char c) -> char ;
+auto Choice_Type_Thecking (unsigned char c) -> unsigned char;
+#endif // TYPE_THECKING_H
