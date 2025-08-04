f3557e7ccac264412738185ea71412ea1fda5004
david44476 <davidov435@mail.ru>
2025-08-04 14:00:12 +0900

    Add files via upload

---
+++ Lab_4/task_8.h
@@ -0,0 +1,33 @@
+#ifndef TASK_8_H
+#define TASK_8_H
+#include <functional>
+
+auto LogicalFunctions () -> void; // деклорация функции для задания № 8
+
+// создаём псевдоним типа для unsigned int
+using ubit_t = unsigned int;
+
+// создаём псевдоним типа для unsigned char
+using uchar_t = unsigned char;
+
+// создаём псевдоним типа указателя на функции
+using binary_op = bool(*)(ubit_t, ubit_t);
+
+// создаём псевдоним типа указателя на функцию std::function
+using funcStd = std::function<bool (ubit_t a, ubit_t b)>;
+
+// функция для логического И (&)
+auto AndOperatin (ubit_t var1, ubit_t var2) -> bool {
+    return var1 & var2;
+}
+
+// функция для логического ИЛИ (|)
+auto OrOperatin (ubit_t var1, ubit_t var2) -> bool {
+    return var1 | var2;
+}
+
+// функция для логического ИСКЛЮЧАЮЩЕГО ИЛИ (^)
+auto XorOperatin (ubit_t var1, ubit_t var2) -> bool {
+    return var1 ^ var2;
+}
+#endif // TASK_8_H
