31233fd46b38e2196fbb4cf9ce7b54306bcf4335
david44476 <davidov435@mail.ru>
2025-08-07 01:13:57 +0900

    Add files via upload

---
+++ Lab_5/main.cpp
@@ -0,0 +1,40 @@
+#define TASK 1
+#if (TASK == 1)
+#include "task_1.h"
+#elif (TASK == 2)
+#include "task_2.h"
+#elif (TASK == 3)
+#include "task_3.h"
+#elif (TASK == 4)
+#include "task_4.h"
+#elif (TASK == 5)
+#include "task_5.h"
+#elif (TASK == 6)
+#include "task_6.h"
+#elif (TASK == 7)
+#include "task_7.h"
+#elif (TASK == 8)
+#include "task_8.h"
+#else
+#endif
+int main() {
+#if (TASK == 1)
+    Task_1();
+#elif (TASK == 2)
+    task_2();
+#elif (TASK == 3)
+    task_3();
+#elif (TASK == 4)
+    task_4();
+#elif (TASK == 5)
+    task_5();
+#elif (TASK == 6)
+    task_6();
+#elif (TASK == 7)
+    task_7();
+#elif (TASK == 8)
+    task_8();
+#else
+// и тд
+#endif
+}
---
+++ Lab_5/type_thecking.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<limits>
+
+// проверка на коректность ввода
+auto Type_Thecking (char c) -> char {
+    while (true) {
+        std::cin >> c;
+        if ( ! (c == 'Y' || c == 'y' || c == 'N' || c == 'n') || c == ' ') {
+            std::cout << "Вы ввели не верный символ!" << '\n' << "Попробуйте ещё: ";
+            std::cin.clear ();
+            std::cin.ignore (std::numeric_limits<char>::max (), '\n');
+        }
+        else break;
+    }
+    return c;
+}
+
+// проверка на коректность ввода
+auto Choice_Type_Thecking (unsigned char c) -> unsigned char {
+    while (true) {
+        std::cin >> c;
+        if ( ! (c == '&' || c == '|' || c == '^')) {
+            std::cout << "Вы ввели не верный символ!" << '\n' << "Попробуйте ещё: ";
+            std::cin.clear ();
+            std::cin.ignore (std::numeric_limits<char>::max (), '\n');
+        }
+        else break;
+    }
+    return c;
+}
+
+// функция для проверки ввода типа char введённое пользователем от A до Z
+auto Char_Type_Thecking (char c) -> char {
+    while (true) {
+        if (! (std::cin >> c) || ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z'))) {
+            std::cout << "Вы ввели не верный символ!" << '\n' << "Попробуйте ещё: ";
+            std::cin.clear ();
+            std::cin.ignore (std::numeric_limits<char>::max (), '\n');
+        }
+        else break;
+    }
+    return c;
+}
