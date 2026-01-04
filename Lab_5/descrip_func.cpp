4a630c1b8ed006f6f247999ba6462e27966df445
david44476 <davidov435@mail.ru>
2026-01-04 21:19:41 +0900

    Update descrip_func.cpp

--- Lab_5/descrip_func.cpp
+++ Lab_5/descrip_func.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -5,7 +6,12 @@
 
 // псевдоним типа
 using wstr = std::wstring;
-auto StrToNum (wstr& fp_str) -> void;
+
+// деклорация функции преобразования строки в число
+auto StrToNum (wstr& fp_str) -> int;
+
+// деклорация функции преобразования числа в строку
+auto intToString(int num) -> wstr;
 
 // функция по заданию №7
 auto DescripFunc () -> RetConst {
@@ -18,13 +24,16 @@ auto DescripFunc () -> RetConst {
                   "и строки в число." <<'\n';
 
     wstr str{L"-70000"};
-    StrToNum (str);
-
+    int num;
+    num = StrToNum (str);
+    std::wcout << num << L" Тип даных: " << typeid(num).name() << std::endl;
+    wstr stroca = intToString(num);
+    std::wcout << stroca << L" Тип даных: " << typeid(stroca).name() << std::endl;
     return Ok;
 }
 
 // функция преобразования строки в число
-auto StrToNum (wstr& fp_str) -> void {
+auto StrToNum (wstr& fp_str) -> int {
 
     int num{0}; // число
     int i{0};
@@ -35,7 +44,7 @@ auto StrToNum (wstr& fp_str) -> void {
         ++i;
     }
     // проверяем, является ли символ цифрой
-    while(std::isdigit(fp_str[i])) {
+    while(std::isdigit (fp_str[i])) {
 
         // добавляем в массив цифру
         num = num * 10 + (fp_str[i] - '0');
@@ -43,5 +52,31 @@ auto StrToNum (wstr& fp_str) -> void {
     }
     if (sign == 1)
         num = -num;
-    std::wcout << num << L" Тип даных: " << typeid(num).name() << std::endl;
+    return num;
+}
+
+// функция преобразования числа в строку
+auto intToString(int num) -> wstr {
+    wchar_t buffer[32]; // хватит для любого int
+    int pos = 0;
+
+    if (num == 0) {
+        buffer[pos++] = '0';
+    } else {
+        bool isNegative = (num < 0);
+        unsigned int absNum = (isNegative) ? -num : num;
+
+        // Заполняем буфер с конца
+        while (absNum > 0) {
+            buffer[pos++] = '0' + (absNum % 10);
+            absNum /= 10;
+        }
+
+        if (isNegative) buffer[pos++] = '-';
+    }
+
+    // Переворачиваем буфер (цифры записаны в обратном порядке)
+    std::reverse(buffer, buffer + pos);
+
+    return wstr(buffer, pos);
 }
