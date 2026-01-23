c3248b8df2d310eb0b0eeabd84ee3bb32b28334a
david44476 <davidov435@mail.ru>
2026-01-23 18:34:54 +0900

    Update mail_address.cpp

--- Lab_5/mail_address.cpp
+++ Lab_5/mail_address.cpp
@@ -14,12 +14,12 @@ struct Address {
     wstr S_Flat; // квартира
 } *address{nullptr};
 
-// void Parse (wstr& line, Address* address);
-// void Unify (Address* address);
+// void Parse (wstr& line, Address *address);
+// void Unify (Address *address);
 // wstr Format(const Address& address);
 
 // функция выделения из строки компонентов адреса
-void Parse (wstr& line, Address* address) {
+void Parse (wstr& line, Address *address) {
     if (line.empty ()) {
         std::wcout << L"exception\n";
     }
@@ -31,8 +31,7 @@ void Parse (wstr& line, Address* address) {
     if (stop == std::string::npos) {
         std::wcout << L"Неверный формат: пропущена запятая\n";
     }
-    address->S_Country = line.substr (start, stop - start);
-    std::wcout << L"Всё в порядке!\n";
+    address->S_Country = line.substr (start, stop);
     start = stop + 1;
 
     // Извлекаем S_City
@@ -68,7 +67,7 @@ void Parse (wstr& line, Address* address) {
 }
 
 // функция приводит компоненты адреса к каноническому виду
-void Unify (Address* address) {
+void Unify (Address *address) {
     if (address == nullptr) {
         std::wcout << L"exception\n";
     }
@@ -77,36 +76,34 @@ void Unify (Address* address) {
     auto pos = address->S_Country.find (L"с-на.", 0);
     if (pos != std::string::npos) {
         address->S_Country.replace (pos, 5, L"страна");
-    } else {
-        address->S_Country.insert (0, L"страна ");
     }
     // Удаление пробелов по краям
-    address->S_Country.erase (0, address->S_House.find_first_not_of (' '));
-    address->S_Country.erase (address->S_House.find_last_not_of (' ') + 1);
+    address->S_Country.erase (0, address->S_Country.find_first_not_of (' '));
+    address->S_Country.erase (address->S_Country.find_last_not_of (' ') + 1);
 
     // Обработка S_City
     pos = address->S_City.find (L"г.", 0);
     if (pos != std::string::npos) {
         address->S_City.replace (pos, 2, L"город");
-    } else {
-        address->S_City.insert (0, L"город ");
     }
     // Удаление пробелов по краям
-    address->S_City.erase (0, address->S_House.find_first_not_of (' '));
-    address->S_City.erase (address->S_House.find_last_not_of (' ') + 1);
+    address->S_City.erase (0, address->S_City.find_first_not_of (' '));
+    address->S_City.erase (address->S_City.find_last_not_of (' ') + 1);
 
     // Обработка S_Street
     pos = address->S_Street.find(L"ул.", 0);
     if (pos != std::string::npos) {
         address->S_Street.replace (pos, 3, L"улица");
     }
+
     pos = address->S_Street.find (L"пр-д.", 0);
     if (pos != std::string::npos) {
         address->S_Street.replace (pos, 5, L"проезд");
     }
-    pos = address->S_Street.find (L"улица", 0);
-    if (pos == std::string::npos) {
-        address->S_Street.insert (0, L"улица ");
+
+    pos = address->S_Street.find (L"мик-н.", 0);
+    if (pos != std::string::npos) {
+        address->S_Street.replace (pos, 6, L"микрорайон");
     }
     address->S_Street.erase (0, address->S_Street.find_first_not_of (' '));
     address->S_Street.erase (address->S_Street.find_last_not_of (' ') + 1);
@@ -126,8 +123,6 @@ void Unify (Address* address) {
     pos = address->S_Flat.find (L"кв.", 0);
     if (pos != std::string::npos) {
         address->S_Flat.replace (pos, 3, L"квартира");
-    } else {
-        address->S_Flat.insert (0, L"квартира ");
     }
     // Удаление пробелов по краям
     address->S_Flat.erase (0, address->S_Flat.find_first_not_of (' '));
@@ -148,17 +143,18 @@ auto MailAddress () -> void {
     wstr s = L"Задание 8)";
     std::wcout << std::setw (30) << std::right << s << '\n';
     std::wcout << L"Разработать программу, обрабатывающую почтовые адреса." << '\n';
-    std::wcout << L"Всё в порядке!\n";
     std::wcout << L"Введите почтовый адрес в формате: " << '\n'
                << L"с-на. Россия, г. Краснокаменск, мик-н. 4, д. 444, кв. 76" << '\n';
 
-    wstr line = L"Россия, г. Краснокаменск, мик-н. Северный, д. 444";
-    //wstr line;
+    //wstr line = L"с-на. Россия, г. Краснокаменск, ул. Северный, д. 444, кв. 76";
 
-    //while (std::getline(std::wcin, line)) {
+    wstr line;
+    address = new (std::nothrow) Address[100];
+
+    while (std::getline(std::wcin, line)) {
 
     Parse(line, address);
     Unify(address);
     std::wcout << Format(*address) << "\n";
-    //}
+    }
 }
