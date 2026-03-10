64a5ffef5adabac834ad4f84733bba92e223741c
david44476 <davidov435@mail.ru>
2026-03-11 07:08:47 +0900

    Delete Lab_6/task_selection.h

--- Lab_6/task_selection.h
+++
@@ -1,7 +0,0 @@
-#ifndef TASK_SELECTION_H
-#define TASK_SELECTION_H
-#include"constans.h"
-
-// деклорация функция выбора задания
-auto TaskSelection(const short& f_choice) -> ProgrEnum;
-#endif // TASK_SELECTION_H
