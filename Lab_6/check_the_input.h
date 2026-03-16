334aa7c522ea0e1d042dd0d5dbfe82e6c4520908
david44476 <davidov435@mail.ru>
2026-03-16 20:34:51 +0900

    Delete Lab_6/check_the_input.h

--- Lab_6/check_the_input.h
+++
@@ -1,7 +0,0 @@
-#ifndef CHECK_THE_INPUT_H
-#define CHECK_THE_INPUT_H
-
-// деклорация функций проверки ввода
-auto Type_Thecking(short& a, const short& min, const short& max ) -> short;
-auto Type_Thecking(wchar_t& f_c) -> wchar_t;
-#endif // CHECK_THE_INPUT_H
