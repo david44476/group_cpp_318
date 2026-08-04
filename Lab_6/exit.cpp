38a57ca1217d0e2c5a29b961876a3e7aef78c5b4
david44476 <davidov435@mail.ru>
2026-08-04 13:27:33 +0900

    Update exit.cpp

--- Lab_6/exit.cpp
+++ Lab_6/exit.cpp
@@ -1,26 +1,30 @@
 #include<iostream>
 #include<limits>
 
+#include<iostream>
+#include<limits>
+
 bool Exit() {
-    std::cout << "Желаете продолжить введите Y, если нет введите N: ";
-    char choice;
+    std::wcout << std::wstring(90, '-') << '\n';
+    std::wcout << L"Желаете продолжить введите Y, если нет введите N: ";
+    wchar_t choice;
     while (true) {
-        if (!(std::cin >> choice)) {
+        if (!(std::wcin >> choice)) {
 
             // ошибка ввода
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            std::cout << "Ошибка ввода: Введите Y или N: ";
+            std::wcin.clear();
+            std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::wcout << L"Ошибка ввода: Введите Y или N: ";
             continue;
         }
 
         // преобразуем символ в верхний регистр
-        choice = std::toupper(choice);
+        choice = std::towupper(choice);
 
-        if (choice == 'Y') {
+        if (choice == L'Y') {
             return true;
-        } else if (choice == 'N') {
+        } else if (choice == L'N') {
             return false;
-        } else std::cout << "Введйн не верный символ! Введите Y или N: ";
+        } else std::wcout << L"Введйн не верный символ! Введите Y или N: ";
     }
 }
