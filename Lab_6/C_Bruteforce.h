7725100efbbec5b9bbbe8d3723b18668441de8ed
david44476 <davidov435@mail.ru>
2026-08-04 13:25:54 +0900

    Update C_Bruteforce.h

--- Lab_6/C_Bruteforce.h
+++ Lab_6/C_Bruteforce.h
@@ -5,18 +5,17 @@
 
 class Bruteforce {
 private:
-    std::string m_alphaBet; // набор допустимых символов
+    std::wstring m_alphaBet; // набор допустимых символов
     unsigned short m_maxLen; // максимальная длина пароля
 public:
 
     // конструктор: задаёт алфавит и максимальную длину пароля
-    Bruteforce(const std::string &alphaBet, const unsigned short &maxLen);
+    Bruteforce(const std::wstring &alphaBet, const unsigned short &maxLen);
 
     // вывод текущих ограничений(алфавит, длина, оценка числа комбинаций)
     void PrintLimits() const;
 
     // перебор: возврошает найденный пароль(гарантированно находит, если он укладывается в ограничения
-    std::string FindPassword(const std::string &target) const;
+    std::wstring FindPassword(const std::wstring &target) const;
 };
-
 #endif // C_BRUTEFORCE_H
